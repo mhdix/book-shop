f93d114673dad2f8e824e33a4925d9284398b68f
mahdi <domanloom@gmail.com>
2023-08-24 00:13:01 +0330

    introduction fixed

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -5,6 +5,7 @@ import img1 from '../../assets/introduction/1.webp'
 import img2 from '../../assets/introduction/2.webp'
 import img3 from '../../assets/introduction/3.webp'
 import icon from '../../assets/icon/icon.png'
+import { BsFillChatDotsFill, BsFillShareFill } from "react-icons/bs";
 const Introduction = () => {
     return (
         <div>
@@ -17,6 +18,7 @@ const Introduction = () => {
                     <div className='introduction-content'>
                         <h5>برترین ها</h5>
                         <h4>بهترین رمان های کره ای و ژاپنی [ترجمه فارسی]</h4>
+                        {tag()}
                         <p>در دنیای پرشتاب امروزی، ایجاد یک روتین صبحگاهی که نحوه زندگی و عملکرد ما را تنظیم می کند، بیش از همیشه مهم است. این مقاله با الهام از اصول کتاب باشگاه پنج صبحی ها به بررسی قدرت باورنکردنی یک روتین صبحگاهی قدرتمند کامل خود راZ ...</p>
                     </div>
                 </div>
@@ -25,10 +27,7 @@ const Introduction = () => {
                     <div className='introduction-content'>
                         <h5>مقالات</h5>
                         <h4>بهترین رمان های کره ای و ژاپنی [ترجمه فارسی]</h4>
-                        <span>
-                            <p>کتاب سراج</p>
-                            <img src={icon} alt="" />
-                        </span>
+                        {tag()}
                         <p>در دنیای پرشتاب امروزی، ایجاد یک روتین صبحگاهی که نحوه زندگی و عملکرد ما را تنظیم می کند، بیش از همیشه مهم است. این مقاله با الهام از اصول کتاب باشگاه پنج صبحی ها به بررسی قدرت باورنکردنی یک روتین صبحگاهی قدرتمند کامل خود راZ ...</p>
                     </div>
                 </div>
@@ -37,6 +36,7 @@ const Introduction = () => {
                     <div className='introduction-content'>
                         <h5>پرفروش ترین ها</h5>
                         <h4>بهترین رمان های کره ای و ژاپنی [ترجمه فارسی]</h4>
+                        {tag()}
                         <p>در دنیای پرشتاب امروزی، ایجاد یک روتین صبحگاهی که نحوه زندگی و عملکرد ما را تنظیم می کند، بیش از همیشه مهم است. این مقاله با الهام از اصول کتاب باشگاه پنج صبحی ها به بررسی قدرت باورنکردنی یک روتین صبحگاهی قدرتمند کامل خود راZ ...</p>
                     </div>
                 </div>
@@ -45,6 +45,7 @@ const Introduction = () => {
                     <div className='introduction-content'>
                         <h5>پرفروش ترین ها</h5>
                         <h4>بهترین رمان های کره ای و ژاپنی [ترجمه فارسی]</h4>
+                        {tag()}
                         <p>در دنیای پرشتاب امروزی، ایجاد یک روتین صبحگاهی که نحوه زندگی و عملکرد ما را تنظیم می کند، بیش از همیشه مهم است. این مقاله با الهام از اصول کتاب باشگاه پنج صبحی ها به بررسی قدرت باورنکردنی یک روتین صبحگاهی قدرتمند کامل خود راZ ...</p>
                     </div>
                 </div>
@@ -54,3 +55,14 @@ const Introduction = () => {
 }
 
 export default Introduction
+
+function tag() {
+    return (
+        <span>
+           <button><BsFillShareFill /></button>
+            <button><BsFillChatDotsFill /></button>
+            <p>کتاب سراج</p>
+            <img src={icon} alt="" />
+        </span>
+    )
+}
